649da1807101942fda665c9500ca9f30b48586ab
Peter Le <Peterle3@csus.edu>
2017-04-17 20:35:04 -0700

    uhhh

---
+++ public/index.js
@@ -0,0 +1,17 @@
+$(".link").click(function(){
+         window.open('https://www.linkedin.com/in/peterle3/', '_blank');       
+});
+
+$(".git").click(function(){
+         window.open('https://github.com/PeterLe3', '_blank');       
+});
+$(".resume").click(function(){
+window.open('https://drive.google.com/open?id=0B2A-FOmRoyVdOHEyZ2NXdDlLaG8');
+});
+
+
+/*
+$(".proj-tab").click(function(){
+$.scrollTo("#proj");
+   });
+*/
