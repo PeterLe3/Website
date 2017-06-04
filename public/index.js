ff39a7708dc76daff92a0335c05c672b78e240d2
Peter Le <Peterle3@csus.edu>
2017-06-03 22:32:56 -0700

    ouch

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 $(".link").click(function(){
-         window.open('https://www.linkedin.com/in/peterle3/', '_blank');       
+ window.open('https://www.linkedin.com/in/peterle3', '_blank');       
 });
 
 $(".git").click(function(){
@@ -9,9 +9,22 @@ $(".resume").click(function(){
 window.open('https://drive.google.com/open?id=0B2A-FOmRoyVdOHEyZ2NXdDlLaG8');
 });
 
+$("i").hover(
+  function() {
+  $(this).addClass("fa-4x");
+  },
+  
+  function(){
+    $(this).removeClass("fa-4x");
+  } 
+  
+);
+
 
 /*
 $(".proj-tab").click(function(){
 $.scrollTo("#proj");
    });
 */
+
+
