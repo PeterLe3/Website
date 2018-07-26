5a0c13b332e032a9aa2c8507fc0ebddbb62c6f1d
Peter Le <Peterle3@csus.edu>
2018-07-25 21:01:58 -0700

    sort of fixed about and proj sec

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var app = express();
+
+
+
+
+
+
+// make the css files public
+app.use(express.static('public'));
+
+
+var router = express.Router();
+
+ 
+
+// sends index.html file to homepage 
+app.get('/', function(req, res) {
+	//use double quotes because single quotes dont work
+	res.sendFile(path.join(__dirname + "/public/index.html"));
+
+});
+// route middleware
+router.use(function(req, res, next) {
+	//log each request to console
+	console.log(req.method, req.url);
+	//contine doing what we were doing and go to route
+	next();
+
+});
+
+
+
+
+
+
+
+
+
+
+app.use('/', router);
+
+app.listen(1337);
+console.log('1337 server is working');
+
+
+
+
